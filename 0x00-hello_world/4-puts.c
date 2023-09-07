f3de520a9e38c3855b41c470a1e40a347e64d0ac
bonny100 <kinuthiaboniface@gmail.com>
2023-09-07 10:10:55 -0700

    dot c files ready for execution

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * We are about to print a puzzle
+ *
+ * It will just print so the return is 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* Start of prgram
+*
+* Just printing so it will return a zero
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
